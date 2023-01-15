381118627deef805f99c802f9cf4c1de066d3455
EVGEN <k.e.l@tut.by>
2023-01-15 14:35:05 +0300

    feat: add server error handling, install jest and supertest

--- src/index.ts
+++ src/index.ts
@@ -13,35 +13,42 @@ const PORT = process.env.PORT || 4000;
 const server = http.createServer();
 
 server.on('request', (request, response) => {
-  const { method, url } = request;
-  if (method === 'GET' && url === '/api/users') {
-    getAllUsers(request, response);
-  } else if (
-    method === 'GET' &&
-    url?.startsWith('/api/users') &&
-    url?.split('/').length === 4
-  ) {
-    const id = url.split('/')[3];
-    getUserById(request, response, id);
-  } else if (method === 'POST' && url === '/api/users') {
-    createUser(request, response);
-  } else if (
-    method === 'PUT' &&
-    url?.startsWith('/api/users') &&
-    url?.split('/').length === 4
-  ) {
-    const id = url.split('/')[3];
-    updateDataUser(request, response, id);
-  } else if (
-    method === 'DELETE' &&
-    url?.startsWith('/api/users') &&
-    url?.split('/').length === 4
-  ) {
-    const id = url.split('/')[3];
-    deleteUser(request, response, id);
-  } else {
-    response.writeHead(404, { 'Content-type': 'application/json' });
-    response.end(JSON.stringify({ message: 'Invalid data in request' }));
+  try {
+    const { method, url } = request;
+    if (method === 'GET' && url === '/api/users') {
+      getAllUsers(request, response);
+    } else if (
+      method === 'GET' &&
+      url?.startsWith('/api/users') &&
+      url?.split('/').length === 4
+    ) {
+      const id = url.split('/')[3];
+      getUserById(request, response, id);
+    } else if (method === 'POST' && url === '/api/users') {
+      createUser(request, response);
+    } else if (
+      method === 'PUT' &&
+      url?.startsWith('/api/users') &&
+      url?.split('/').length === 4
+    ) {
+      const id = url.split('/')[3];
+      updateDataUser(request, response, id);
+    } else if (
+      method === 'DELETE' &&
+      url?.startsWith('/api/users') &&
+      url?.split('/').length === 4
+    ) {
+      const id = url.split('/')[3];
+      deleteUser(request, response, id);
+    } else {
+      response.writeHead(404, { 'Content-type': 'application/json' });
+      response.end(JSON.stringify({ message: 'Invalid data in request' }));
+    }
+  } catch {
+    response.writeHead(500, { 'Content-type': 'application/json' });
+    response.end(
+      JSON.stringify({ message: 'Sorry, server error, please try again' })
+    );
   }
 });
 
